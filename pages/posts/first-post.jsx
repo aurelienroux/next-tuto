a0726d848cbb1c96464283a1f3fd13ed0b27ce13
aurelienroux <aurelien.roux@busbud.com>
2023-10-13 16:21:19 -0400

    second page & image

---
+++ pages/posts/first-post.jsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function FirstPost() {
+  return (
+    <div>
+      <Head>
+        <title>First post</title>
+      </Head>
+
+      <h1>First Post</h1>
+      <Link href="/">Return to home page</Link>
+
+      <Image src="/images/avatar.png" width="150" height="150" alt="avatar" />
+    </div>
+  );
+}
